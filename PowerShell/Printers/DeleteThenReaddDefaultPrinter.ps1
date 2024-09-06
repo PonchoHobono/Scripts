eeca7171116b087e191637da3c8c6cfb0057775f
PonchoHobono <ponchohobono@gmail.com>
2024-09-06 03:15:32 -0400

    Delete PowerShell/Printers directory

--- PowerShell/Printers/DeleteThenReaddDefaultPrinter.ps1
+++
@@ -1,8 +0,0 @@
-# This is an old way of doing things.
-
-$Net = New-Object -Com WScript.Network
-$DefaultPrinter = Get-WMIObject -Class Win32_Printer -computer . -Filter Default=True
-$DefaultPrinter
-$DefaultPrinter.Delete()
-$Net.AddWindowsPrinterConnection($DefaultPrinter.DeviceId)
-$DefaultPrinter.SetDefaultPrinter()
